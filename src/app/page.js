340084d3e23af008427b5a6aec281da2fbf6b890
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2025-01-07 16:00:27 +0500

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import Products from "@/components/Products";
-import Services from "@/components/Services";
-import Footer from "@/components/Footer";
-import Enquiry from "@/components/Enquiry";
-import Members from "@/components/Members";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import Products from "../components/Products";
+import Services from "../components/Services";
+import Footer from "../components/Footer";
+import Enquiry from "../components/Enquiry";
+import Members from "../components/Members";
 
 export default function Home() {
   return (
