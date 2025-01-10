c041d60a9e040d76c6392b420419f702b91218fd
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2025-01-10 22:02:30 +0500

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <AboutSection />
       <Products />
       <Services />
-      {/* <Members /> */}
+      <Members />
       <Enquiry />
       <Footer />
     </>
